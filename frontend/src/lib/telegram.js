b9560429a68ea30cd73d9700136eeeb22fbdd653
bukamal <manhal.almasriiii199119@gmail.com>
2026-04-19 20:44:06 +0300

    إضافة showConfirm و hapticFeedback إلى telegram.js

--- frontend/src/lib/telegram.js
+++ frontend/src/lib/telegram.js
@@ -8,6 +8,22 @@ export const initTelegram = () => {
   return null
 }
 
-export const hapticFeedback = (type) => {
+export const hapticFeedback = (type = 'light') => {
   window.Telegram?.WebApp?.HapticFeedback?.impactOccurred(type)
+}
+
+export const showAlert = (message) => {
+  if (window.Telegram?.WebApp) {
+    window.Telegram.WebApp.showAlert(message)
+  } else {
+    alert(message)
+  }
+}
+
+export const showConfirm = (message, callback) => {
+  if (window.Telegram?.WebApp) {
+    window.Telegram.WebApp.showConfirm(message, callback)
+  } else {
+    callback(confirm(message))
+  }
 }
